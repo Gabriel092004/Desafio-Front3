4722f0835f7196b7428309d3c0235c8195b5c6d8
mayantunes <mysantunes@gmail.com>
2022-09-10 19:53:13 -0300

    Tentativa 2

--- src/Components/Header/logoff/index.jsx
+++ src/Components/Header/logoff/index.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { getItem, removeItem } from "../../../utils/storage";
 
 export default async function Logoff() {
 
     const { token, usuario } = getItem('usuario')
 
+    const navigate = useNavigate();
+
     removeItem('usuario');
     removeItem('token');
+
+    navigate('/');
 }
