4edb0df34dc8c40566ca5a20b3b2f11818dd5067
Sidney Silveira <sid.silveiras@hotmail.com>
2022-09-13 18:35:44 -0300

    Atualização do CSS do Main

---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,16 @@
+import './styles.css';
+import Header from '../../Components/Header'
+
+
+function Main() {
+    return (
+        <>
+            <Header />
+            <div className='transacoes'>
+
+            </div>
+        </>
+    )
+}
+
+export default Main;
