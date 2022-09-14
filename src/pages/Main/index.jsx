4df9ae3a1a163f4ef93f8578f0f4e078d7d40b8f
Sidney Silveira <sid.silveiras@hotmail.com>
2022-09-13 21:11:46 -0300

    Atualização de Layout de Main

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,15 +1,17 @@
 import './styles.css';
-import Header from '../../Components/Header'
+import Header from '../../Components/Header';
+import Resumo from '../../Components/Resumo';
 
 
 function Main() {
     return (
-        <>
+        <div className='main'>
+
             <Header />
             <div className='transacoes'>
 
             </div>
-        </>
+        </div>
     )
 }
 
