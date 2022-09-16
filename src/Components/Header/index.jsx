6434fac691c8accb51c32b03597a91acd1e8d6d8
Sidney Silveira <sid.silveiras@hotmail.com>
2022-09-16 09:41:55 -0300

    Implementação do Logout no Header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,10 +1,19 @@
-import Logo from '../../assets/logo.svg'
-import Logout from '../../assets/logout.svg'
-import Profile from '../../assets/user.svg'
-import './styles.css'
+import { useNavigate } from 'react-router-dom';
+import Logo from '../../assets/logo.svg';
+import Logout from '../../assets/logout.svg';
+import Profile from '../../assets/user.svg';
+import { clear } from '../../utils/storage';
+import './styles.css';
 
 
 const Header = () => {
+    const navigate = useNavigate()
+
+    function logout() {
+        clear()
+        navigate('/')
+    }
+
     return (
         <div className='container-header'>
             <div className='logo'>
@@ -14,7 +23,7 @@ const Header = () => {
             <div className='images'>
                 <img src={Profile} />
                 <span>Nome do Usuário</span>
-                <img src={Logout} />
+                <img className='logout' src={Logout} onClick={() => logout()} />
             </div>
         </div>
 
